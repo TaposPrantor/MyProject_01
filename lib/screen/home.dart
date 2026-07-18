bc5e712ed18f17355688c56e9ac06d782d7fcfa2
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-18 11:04:54 +0600

    Note App 1

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -16,43 +16,59 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: MyTextWidget(title: "My Notes App" , tColor: Colors.blue,),
+        title: MyTextWidget(title: "My Notes App", tColor: Colors.blue),
         backgroundColor: Color(0xff27F5E4),
       ),
       body: ListView.builder(
         itemCount: NoteData.note.length,
-          itemBuilder: (context, i)=> Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-            child: ListTile(
-              leading: CircleAvatar(child: MyTextWidget(title: "${i+1} "),),
-              tileColor: Colors.grey.shade300,
-              title: MyTextWidget(title: "${NoteData.note[i]['title']}"),
-              subtitle: MyTextWidget(title: "${NoteData.note[i]['details']}"),
-              trailing: InkWell(
-                onTap: () {
-                  NoteData.note.removeAt(i);
-                  setState(() {
-
-                  });
-                },
-                child: Icon(Icons.delete, color: Colors.red,),
-              ),
+        itemBuilder: (context, i) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          child: ListTile(
+            leading: CircleAvatar(child: MyTextWidget(title: "${i + 1} ")),
+            tileColor: Colors.grey.shade300,
+            title: MyTextWidget(title: "${NoteData.note[i]['title']}"),
+            subtitle: MyTextWidget(title: "${NoteData.note[i]['details']}"),
+            trailing: InkWell(
+              onTap: () {
+                NoteData.note.removeAt(i);
+                setState(() {});
+              },
+              child: Icon(Icons.delete, color: Colors.red),
             ),
-          )
+          ),
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(onPressed: () async{
-        showDialog(context: context, builder: (context)=>
-            AlertDialog(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
               title: MyTextWidget(title: "Alert Dialogue"),
-              content: MyTextWidget(title: "Note Title"),
-            )
-        );
-
-
-        // NoteData.note.add({"title" : "Adda", "details" : "Tea=40, Coffee=120"},);
-        // setState(() {});
-      }, child: Icon(Icons.add),),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  MyTextWidget(title: "Note Title"),
+                  MyTextWidget(title: "Note Title"),
+                ],
+              ),
+              actions: [
+                ElevatedButton(
+                  onPressed: () {
+                    NoteData.note.add({
+                      "title": "Adda",
+                      "details": "Tea=40, Coffee=120",
+                    });
+                    setState(() {});
+                  },
+                  child: Text("Add"),
+                ),
+              ],
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
